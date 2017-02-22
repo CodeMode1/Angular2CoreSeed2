4fd4a0c9ea3185d3a890f93b0ab6a113e8bb36c7
DaphDC <daphnedc21@gmail.com>
2017-02-22 18:14:31 -0500

    UI design

--- Angular2CoreSeed/ClientApp/app/components/trip/trip.component.ts
+++ Angular2CoreSeed/ClientApp/app/components/trip/trip.component.ts
@@ -3,7 +3,7 @@ import { ITrip, Trip } from './trip';
 import { TripService } from './trip.service';
 import { Subscription } from 'rxjs/Subscription';
 import { LoginService } from '../login/login.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
     selector: 'trip',
@@ -17,7 +17,7 @@ export class TripComponent {
     public userName: string;
 
     constructor(public _tripService: TripService, private _loginService: LoginService,
-        private _activatedRoute: ActivatedRoute) {
+        private _activatedRoute: ActivatedRoute, private _router: Router) {
         this.titreTrip = "Les Forfaits Voyages";
         this.trips = [];
         if (this._loginService.userName != null && typeof (this._loginService.userName) != "undefined") {
@@ -29,6 +29,10 @@ export class TripComponent {
 
     ngOnInit() {
         this.getAllTrips();
+    }
+
+    home() {
+        this._router.navigateByUrl('/home');
     }
 
     //  get methos sur service.
