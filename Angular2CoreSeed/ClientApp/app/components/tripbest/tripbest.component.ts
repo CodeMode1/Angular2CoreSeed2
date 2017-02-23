c3a31cc789732d4f4f21a614a6c969069bfc5876
DaphDC <daphnedc21@gmail.com>
2017-02-22 23:55:37 -0500

    paging grid telerik

--- Angular2CoreSeed/ClientApp/app/components/tripbest/tripbest.component.ts
+++ Angular2CoreSeed/ClientApp/app/components/tripbest/tripbest.component.ts
@@ -3,6 +3,7 @@ import { ITrip, Trip } from '../trip/trip';
 import { TripService } from '../trip/trip.service';
 import { Subscription } from 'rxjs/Subscription';
 import { ActivatedRoute, Router } from '@angular/router';
+import { GridDataResult, PageChangeEvent } from '@progress/kendo-angular-grid';
 
 @Component({
     selector: 'best-trip',
@@ -11,14 +12,20 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 
 export class BestTripComponent {
+    public gridView: GridDataResult;
     public bestTrips: Trip[];
+    public pageSize: number;
+    public skip: number;
     public isTrips: boolean;
     public titleBestTrips: string;
     private hiddenColumns: string[];
     private columns: string[];
+    public data: Object[];
 
     constructor(public _tripService: TripService, private _activatedRoute: ActivatedRoute, private _router: Router) {
         this.bestTrips = [];
+        this.pageSize = 2;
+        this.skip = 0;
         this.hiddenColumns = [];
         this.columns = ["id"];
         this.isTrips = false;
@@ -31,6 +38,18 @@ export class BestTripComponent {
             this.titleBestTrips = "Consulter";
             this.getBestTrips();
         }
+    }
+
+    pageChange(event: PageChangeEvent): void {
+        this.skip = event.skip;
+        this.loadItems();
+    }
+
+    loadItems() {
+        this.gridView = {
+            data: this.bestTrips.slice(this.skip, this.skip + this.pageSize),
+            total: this.bestTrips.length
+        };
     }
 
     restoreColumns(): void {
@@ -49,8 +68,9 @@ export class BestTripComponent {
     getBestTrips(): Subscription {
         return this._tripService.getBestTripsAPI()
             .subscribe(
-                data => {
+            data => {
                     this.bestTrips = data;
+                    this.loadItems();
                     console.log("success getting best trips : " + data);
                 },
                 error => {
